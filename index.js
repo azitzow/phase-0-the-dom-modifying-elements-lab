f60b3e4607111bcdf93148ae1e73d48c9281c23a
Ana Zitzow <anasempai89@gmail.com>
2022-05-03 20:24:50 -0700

    commit tree 68f228cc6d3c61e246f20d389c53ddf90c58f0fb

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "Ana is the champion";
+
+const element = document.getElementById("main");
+
+element.remove()
+document.body.append(newHeader);
